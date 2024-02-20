ec661d648d693f8ee347b38008311122f7f7bf6b
MuhdHanish <muhammedhanish11@gmail.com>
2024-02-20 16:30:26 +0530

    server setup added session middleware added mongodb connnection using mongoose

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 // Importing express module 
-const express = require("express") 
+const express = require("express");
+// Import express-session module
+const session = require("express-session");
+// Import mongoose module
+const mongoose  = require("mongoose");
 
 // Setting up the server 
-var app = express() 
+const app = express();
 
+// Connect mongodb using mongoose
+mongoose.connect(`mongodb://127.0.0.1:27017/session_cookies`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(_res => console.log(`MongoDB connected successfully 🟢`))
+    .catch(error => console.log(`Unable to connect to MongoDB 🔴`, error));
 
+// Middleware
+app.use(session({
+    secret: `Hello World`,
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Handlers
 app.get(`/`, (req, res) => {
+    req.session.isAuth = true;
     res.send(`Hello World`);
 })
 
 // Server setup 
-app.listen(3000, () => { 
-    console.log(`Server is Running on http://localhost:3000`);
-})
+app.listen(3000, () => {
+    console.log(`Server is Running on http://localhost:3000 🚀`);
+});
