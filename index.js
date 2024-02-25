a29a95bec868e21f6f7a330b04529e3b6167c0c0
MuhdHanish <muhammedhanish11@gmail.com>
2024-02-25 12:34:39 +0530

    changed secret and dashboard button style

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // - session 
 app.use(session({
-    secret: `Hello World`,
+    secret: `session_cookies`,
     resave: false,
     saveUninitialized: false,
     store
